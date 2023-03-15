91903f9db44b3b7dea409e391aa0afc3900fe134
MAteusz Dorosz <126687548+MDorosz@users.noreply.github.com>
2023-03-15 13:26:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+   int x=1;
+  
+    int suma;
+   System.out.print(+ x + ", ");
+      
+  for(int i=1; i<50; i++)
+    {
+    suma = 2*x + 1;
+    System.out.print(+ suma + ", ");
+    x++;
+    
+    }
+  }
+}
